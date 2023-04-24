5187947d178af4fb36e9df26f1271bbd8e060474
Gordon Ivy Poole <gpoole@ooze.attlocal.net>
2023-04-24 01:29:41 -0400

    tweening and improved input handling

--- scenes/player.gd
+++ scenes/player.gd
@@ -7,28 +7,64 @@ extends Node2D
 @onready var e: RayCast2D = $E
 @onready var w: RayCast2D = $W
 
+var ready_to_move: bool = true
+
+enum MovementDirection {
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST,
+	NONE
+	}
+
+var moving: MovementDirection = MovementDirection.NONE
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	astar.set_point_disabled(cellmap.get_cell_id(position))
 
-func _unhandled_input(event: InputEvent) -> void:
+func handle_movement() -> void:
+	if not ready_to_move:
+		return
+		
 	var dv: Vector2 = Vector2()
-	if event.is_action_pressed("move_north"):
+	if moving == MovementDirection.NORTH:
 		dv += Vector2(0, -1)
-	elif event.is_action_pressed("move_south"):
+	elif moving == MovementDirection.SOUTH:
 		dv += Vector2(0, 1)
-	elif event.is_action_pressed("move_east"):
+	elif moving == MovementDirection.EAST:
 		dv += Vector2(1, 0)
-	elif event.is_action_pressed("move_west"):
+	elif moving == MovementDirection.WEST:
 		dv += Vector2(-1, 0)
+	elif moving == MovementDirection.NONE:
+		dv = Vector2.ZERO
 
 	if dv != Vector2.ZERO:
 		dv *= cellmap.CELL_SIZE
 		if not astar.is_point_disabled(cellmap.get_cell_id(position+dv)):
 			astar.set_point_disabled(cellmap.get_cell_id(position), false)
-			position = position+dv
-			astar.set_point_disabled(cellmap.get_cell_id(position))
+			astar.set_point_disabled(cellmap.get_cell_id(position+dv), true)
+#			position = position+dv
+			ready_to_move = false
+			await create_tween().tween_property(self, 'position', position+dv, 0.1).finished
+			ready_to_move = true
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("move_north"):
+		moving = MovementDirection.NORTH
+	elif event.is_action_pressed("move_south"):
+		moving = MovementDirection.SOUTH
+	elif event.is_action_pressed("move_east"):
+		moving = MovementDirection.EAST
+	elif event.is_action_pressed("move_west"):
+		moving = MovementDirection.WEST
+
+	if event.is_action_released("move_north") and moving == MovementDirection.NORTH \
+		or event.is_action_released("move_south") and moving == MovementDirection.SOUTH \
+		or event.is_action_released("move_east") and moving == MovementDirection.EAST \
+		or event.is_action_released("move_west") and moving == MovementDirection.WEST:
+		moving = MovementDirection.NONE
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta) -> void:
-	pass
+func _physics_process(delta) -> void:
+	handle_movement()
